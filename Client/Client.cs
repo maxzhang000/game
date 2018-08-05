9c9ba6bb8e9e185b55b51cc04b32c3491b35bfce
Max Zhang <maxzhang000@gmail.com>
2018-08-04 19:06:08 -0700

    clientServerV1

---
+++ Client/Client.cs
@@ -0,0 +1,45 @@
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+
+
+public class Client {
+
+    public static void Main() {
+        Console.WriteLine("Client Start");
+        try {
+            TcpClient tcpclnt = new TcpClient();
+            Console.WriteLine("Connecting.....");
+            
+            tcpclnt.Connect("192.168.7.91",8001);
+            // use the ipaddress as in the server program
+            
+            Console.WriteLine("Connected");
+            Console.Write("Enter the string to be transmitted : ");
+            
+            String str=Console.ReadLine();
+            Stream stm = tcpclnt.GetStream();
+                        
+            ASCIIEncoding asen= new ASCIIEncoding();
+            byte[] ba=asen.GetBytes(str);
+            Console.WriteLine("Transmitting.....");
+            
+            stm.Write(ba,0,ba.Length);
+            
+            byte[] bb=new byte[100];
+            int k=stm.Read(bb,0,100);
+            
+            for (int i=0;i<k;i++)
+                Console.Write(Convert.ToChar(bb[i]));
+            
+            tcpclnt.Close();
+        }
+        
+        catch (Exception e) {
+            Console.WriteLine("Client Error..... " + e.StackTrace);
+        }
+    }
+
+}
---
+++ Server/Server.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+public class Server{
+
+    public static void Main(){
+        Console.WriteLine("Server Start");
+        try {
+            IPAddress ipAd = IPAddress.Parse("192.168.7.91");
+        
+            TcpListener myList = new TcpListener(ipAd,8001);
+            
+            myList.Start();
+
+            Console.WriteLine("The server is running at port 8001...");    
+            Console.WriteLine("The local End point is  :" + myList.LocalEndpoint );
+            Console.WriteLine("Waiting for a connection.....");
+
+                
+            Socket s=myList.AcceptSocket();
+            Console.WriteLine("Connection accepted from " + s.RemoteEndPoint);
+            
+            byte[] b=new byte[100];
+            int k=s.Receive(b);
+            Console.WriteLine("Recieved...");
+            for (int i=0;i<k;i++)
+                Console.Write(Convert.ToChar(b[i]));
+
+            ASCIIEncoding asen=new ASCIIEncoding();
+            s.Send(asen.GetBytes("The string was recieved by the server."));
+            Console.WriteLine("\nSent Acknowledgement");
+
+            s.Close();
+            myList.Stop();
+                
+        }
+        catch (Exception e) {
+            Console.WriteLine("Server Error..... " + e.StackTrace);
+        } 
+    }
+}
